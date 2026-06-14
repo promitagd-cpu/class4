b3b663f00ec9b9b1a83ffafbbe7cee08dd0fd9c5
promitagd-cpu <promitagd@gmail.com>
2026-06-14 10:46:01 +0530

    Add files via upload

---
+++ class4ac1.py
@@ -0,0 +1,9 @@
+tree1 = 5
+tree2 = 2
+tree3 = 8
+tree4 = 4
+tree5 = 1
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+print (sum)
+average = sum/5
+print (average)
---
+++ class4ac3.py
@@ -0,0 +1,7 @@
+math = int(input("math :"))
+science = int(input("science :"))
+english = int(input("english :"))
+hindi = int(input("hindi :"))
+total = math + science + english + hindi
+percentage = (total/400)* 100
+print (percentage)
